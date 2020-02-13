5197646d4eac6ad92182a506c44339164d3bfdde
syall <s.yuan.all@gmail.com>
2020-02-13 16:31:16 -0500

    Format for Column

--- test.ts
+++ test.ts
@@ -1,4 +1,5 @@
-export const test = (msg: string, f: Function) => console.log(f() ? '✓' : '✗', msg);
+export const test = (msg: string, f: Function) =>
+    console.log(f() ? '✓' : '✗', msg);
 export const testGroup = (msg: string, f: Function) => {
     console.group(msg);
     f();
